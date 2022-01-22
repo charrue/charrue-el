9982e848b77561ba8bad5876c4f20bb1d2aa277a
ckangwen <2437037469@qq.com>
2022-01-22 23:09:40 +0800

    feat(layout): 将layout组件打包后的文件作为入口文件

--- packages/layout/src/index.js
+++ packages/layout/src/index.js
@@ -1,17 +1,7 @@
-import _Layout from "./Layout.vue";
-import _GlobalHeader from "./GlobalHeader.vue";
-import _ContentView from "./ContentView.vue";
-import _GlobalAside from "./GlobalAside.vue";
+import Layout from "./Layout.vue";
 
-[_Layout, _GlobalHeader, _ContentView].forEach((comp) => {
-  comp.install = (app) => {
-    app.component(comp.name, comp);
-  };
-});
-
-export const Layout = _Layout;
-export const GlobalHeader = _GlobalHeader;
-export const ContentView = _ContentView;
-export const GlobalAside = _GlobalAside;
+Layout.install = (app) => {
+  app.component(Layout.name, Layout);
+};
 
 export default Layout;
