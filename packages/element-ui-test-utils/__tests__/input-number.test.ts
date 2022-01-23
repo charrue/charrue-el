e41cebc9b8c9aeddee98408a614d5214817f964d
ckangwen <2437037469@qq.com>
2022-01-23 12:51:18 +0800

    feat(element-ui-test-utils): 完善input-number的test utils

---
+++ packages/element-ui-test-utils/__tests__/input-number.test.ts
@@ -0,0 +1,95 @@
+import { InputNumber } from "element-ui";
+import { createInstance } from '../src/common';
+import { createElInputNumberTestUtils } from '../src/input-number';
+
+const createElInputInstance = (options = {}) => {
+  return createInstance({
+    components: {
+      'el-input-number': InputNumber,
+    },
+    ...options,
+  })
+}
+
+describe('createElInputNumberTestUtils', () => {
+  it('getValue', () => {
+    const { wrapper } = createElInputInstance({
+      template: `<el-input-number v-model="num" :min="1" :max="10" label="描述文字"></el-input-number>`,
+      data() {
+        return {
+          num: 1
+        }
+      }
+    })
+    const { getValue } = createElInputNumberTestUtils(wrapper)
+    expect(getValue()).toBe(1)
+  });
+
+  it('setValue', async () => {
+    const { wrapper } = createElInputInstance({
+      template: `<el-input-number v-model="num" :min="1" :max="10" label="描述文字"></el-input-number>`,
+      data() {
+        return {
+          num: 7
+        }
+      }
+    })
+    const { getValue, setValue } = createElInputNumberTestUtils(wrapper)
+    await setValue(9)
+    expect(getValue()).toBe(9)
+  });
+
+  it('increase', async () => {
+    const { wrapper } = createElInputInstance({
+      template: `<el-input-number v-model="num" :min="1" :max="10" label="描述文字"></el-input-number>`,
+      data() {
+        return {
+          num: 8
+        }
+      }
+    })
+    const { getValue, increase, listen } = createElInputNumberTestUtils(wrapper)
+    const onInput = jest.fn()
+    listen('input', onInput)
+
+    await increase()
+    expect(onInput).toBeCalledTimes(2)
+    expect(getValue()).toBe(9)
+
+    await increase()
+    expect(onInput).toBeCalledTimes(4)
+    expect(getValue()).toBe(10)
+
+    await increase()
+    await increase()
+    expect(getValue()).toBe(10)
+    expect(onInput).toBeCalledTimes(4)
+  });
+
+  it('decrease', async () => {
+    const { wrapper } = createElInputInstance({
+      template: `<el-input-number v-model="num" :min="1" :max="10" label="描述文字"></el-input-number>`,
+      data() {
+        return {
+          num: 3
+        }
+      }
+    })
+    const { getValue, decrease, listen } = createElInputNumberTestUtils(wrapper)
+    const onInput = jest.fn()
+    listen('input', onInput)
+
+    await decrease()
+    expect(onInput).toBeCalledTimes(2)
+    expect(getValue()).toBe(2)
+
+    await decrease()
+    expect(onInput).toBeCalledTimes(4)
+    expect(getValue()).toBe(1)
+
+    await decrease()
+    await decrease()
+    expect(getValue()).toBe(1)
+    expect(onInput).toBeCalledTimes(4)
+  });
+});
---
+++ packages/element-ui-test-utils/src/input-number.ts
@@ -0,0 +1,45 @@
+import type { ElWrapper, ElVm } from './common';
+
+export const createElInputNumberTestUtils = (wrapper: ElWrapper) => {
+  const compWrapper = wrapper.findComponent({ name: 'el-input-number' });
+  const compVm = compWrapper.vm as ElVm
+
+  const getValue = (): number => {
+    return parseInt(compVm.value, 10)
+  }
+  const listen = (eventName: string, cb: () => void) => compVm.$on(eventName, cb)
+
+  return {
+    getWrapper(): ElWrapper {
+      return compWrapper as ElWrapper
+    },
+    getVm(): ElVm {
+      return compVm
+    },
+    listen,
+    getValue(): number {
+      return getValue()
+    },
+    async setValue(value: number): Promise<void> {
+      const inputWrapper = compWrapper.find<HTMLInputElement>('.el-input__inner');
+      inputWrapper.setValue(value)
+      inputWrapper.trigger('change')
+      await compVm.$nextTick()
+    },
+    async decrease(): Promise<void> {
+      const decreaseWrapper = compWrapper.find('.el-input-number__decrease')
+      // el-input-number的decrease的点击是通过v-repeat-click指令实现的，即mouseup、mousedown事件的组合
+      // 如果两个事件的时间间隔在100ms内，则会执行一次事件函数
+      decreaseWrapper.trigger('mousedown')
+      document.dispatchEvent(new Event("mouseup"))
+      await compVm.$nextTick()
+    },
+
+    async increase(): Promise<void> {
+      const increaseWrapper = compWrapper.find('.el-input-number__increase')
+      increaseWrapper.trigger('mousedown')
+      document.dispatchEvent(new Event("mouseup"))
+      await compVm.$nextTick()
+    },
+  }
+}
